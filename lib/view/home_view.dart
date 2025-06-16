89912f46756c6d9f88f5455f46eef11768da2abc
jordanbu <burgosjordan194@gmail.com>
2025-06-16 17:11:16 -0400

    cambios login

--- lib/view/home_view.dart
+++ lib/view/home_view.dart
@@ -1,10 +1,28 @@
 import 'package:flutter/material.dart';
+import 'login_view.dart'; // Asegúrate de tener el import correcto
 
 class HomeView extends StatelessWidget {
+  const HomeView({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Inicio")),
+      appBar: AppBar(
+        title: Text("Inicio"),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.logout),
+            tooltip: 'Cerrar sesión',
+            onPressed: () {
+              // Regresa a la pantalla de login
+              Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(builder: (context) => LoginView()),
+              );
+            },
+          ),
+        ],
+      ),
       body: Center(
         child: Text(
           'Bienvenido a Widson CRM',
